3db05d7a9f0285064cc291d74d341e8cbc0282a2
sisqodataclub <fd92uk@gmail.com>
2026-08-03 15:05:36 +0100

    Auto-commit: 2026-08-03 15:05:36

---
+++ apps/forms/backend/cv/views/__init__.py
@@ -0,0 +1,11 @@
+# cv/views/__init__.py
+from .cv_views import (
+    react_demo_view,
+    ResumeViewSet,
+    JobApplicationViewSet,
+)
+from .ai_views import (
+    analyze_cv,
+    stream_mission,
+    fetch_cv_report,
+)
